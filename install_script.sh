1036a3f6470a8471ad3c8a32dceb686930bb2961
jared <jared.marks@live.com>
2020-04-21 16:59:37 -0400

    updated install script

--- install_script.sh
+++ install_script.sh
@@ -10,10 +10,6 @@ nvm alias default v12.7.0
 nvm install v12.7.0
 nvm use v12.7.0
 
-wget https://github.com/jmarks199/web_app/archive/master.zip
-unzip master.zip
-mv web-app-master app
-
 mkdir -p /home/ec2-user/app/logs
 cd app
 npm install
