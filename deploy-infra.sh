8c8cd6c00d277f22c804269a41325e91e96d0dae
EC2 Default User <ec2-user@ip-172-31-94-52.ec2.internal>
2020-04-22 16:15:23 +0000

    fixed deploy script

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -9,7 +9,6 @@ EC2_INSTANCE_TYPE=t2.micro
 echo -e "\n\n========== Deploying main.yml ============"
 aws cloudformation deploy \
   --region $REGION \
-  #--profile $CLI_PROFILE \
   --stack-name $STACK_NAME \
   --template-file main.yml \
   --no-fail-on-empty-changeset \
